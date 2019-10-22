5b4e5e0484ec4f6f1c98b3bfac1e8b59db67db72
Alice Freda <alicefreda@gmail.com>
2019-10-22 21:29:42 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    string = self.squeeze(”.”)
+    string = string.squeeze(”!”)
+    binding.pry
+    string.split(/[$.|?|!]/).count
 
   end
 end
