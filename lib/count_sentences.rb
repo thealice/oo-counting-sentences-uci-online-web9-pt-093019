f4ec00242456f588e0eaa9c476663f162ba3d63c
Alice Freda <alicefreda@gmail.com>
2019-10-22 21:37:39 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,11 +15,10 @@ class String
   end
 
   def count_sentences
-
-    string_array = []
-    self.split(" ")
-    string_array << self
-    string_array.count
+    # string_array = []
+    self.split(" ").count
+    # string_array << self
+    # string_array.count
 
   end
 end
