2989355c0189e510fa3a5099f8a617eb04e1a2a7
Alice Freda <alicefreda@gmail.com>
2019-10-22 22:18:47 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    new_string = self.tr(","," ")
+    new_string = self.squeeze(".")
+    new_string = new_string.squeeze("!")
+    new_string.split(/[\.|?|!]/).count
   end
 end
